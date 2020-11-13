d840f04c79e29ba18f54b0fb51d205fe6dae9d62
Baptiste Fourmont <baptiste_fourmont@tutanota.com>
2020-11-13 14:51:31 +0100

    Ajout Objet Fixe

--- Obstacle.java
+++ Obstacle.java
@@ -3,6 +3,9 @@ public class Obstacle{
         Correspond aux animaux à sauver: Petit chat
                                          Petit Cochon 1000points par petit cochon
                                          Petit chien
+        Certains objets sont fixes et ne peuvent pas bouger:    Planche
+                                                                Bois
+
     */
 
 }
