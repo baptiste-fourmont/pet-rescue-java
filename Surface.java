4f93827d7161fff2c36fa8b226b19c1af7dc39a4
Baptiste Fourmont <baptiste_fourmont@tutanota.com>
2020-11-13 14:20:00 +0100

    Ajout Algorithme surface

--- Surface.java
+++ Surface.java
@@ -18,6 +18,14 @@ public interface Surface {
         1690            13
 
         Chaque couleur a le même nombre de point 
-        On peut supprimer les 
+        On peut supprimer quand y en a au moins deux à côté l'un de l'autres
+        Algorithme pour les points: 
+                Une case vaut 10 même si on peut pas la suppriemr c'est sur
+                Multiple de la case 2*2 = 4         et on rjaoute un 0 à la fin a chaque fois
+                                    3*3 = 9
+                                    4*4 = 16
+                                    5*5 = 25
+                                    ..
+                                    13*13= 169
     */
 }
