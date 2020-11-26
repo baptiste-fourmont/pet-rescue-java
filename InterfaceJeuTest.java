13c4387d5b4c537919811993f19f7f5621398555
Dany Hamimi <dany.hamimi@gmail.com>
2020-11-26 14:54:47 +0100

    J'essaye d'apprendre à faire fonctionner les interfaces graphiques pour essayer de faire une grille ( Ensuite faudra penser à faire les textures, écran d'accueil...)

--- InterfaceJeuTest.java
+++ InterfaceJeuTest.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -13,14 +14,16 @@ public class InterfaceJeuTest extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("FruttySaver"); // On changera le nom plus tard ptdr
+        primaryStage.setTitle("hehe rescue"); // On changera le nom plus tard ptdr
         BorderPane borderPaneRoot = new BorderPane();
-        Scene scene = new Scene(borderPaneRoot,800,600);
+        Scene scene = new Scene(borderPaneRoot,800,800);
         primaryStage.setScene(scene);
         HBox hbox1 = new HBox();
         Label labelNom = new Label("Coucou Baptiste:");
         hbox1.getChildren().addAll(labelNom);
         borderPaneRoot.setCenter(hbox1);
+        GridPane gridPane = new GridPane();
+
         primaryStage.show();
     }
 }
